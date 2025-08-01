f5ead4ba42ed7d98afaa40dd62b49820efa73c5a
UBER-VIDEO <yashvloggs20@gmail.com>
2025-08-01 17:41:27 +0530

    Added PriceSection Ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from '@/components/home/hero-section';
 import BgGradient from '@/components/common/bg-gradient';
 import DemoSection from '@/components/home/demo-section';
 import HowItWorksSection from '@/components/common/how-it-works-sections';
+import PricingSection from '@/components/home/pricing-section';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <HeroSection />
         <DemoSection />
         <HowItWorksSection />
+        <PricingSection />
       </div>
 
       
